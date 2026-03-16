0b1ec7eeeaf999a78d8d55d0a2e1ceef37a76da2
tangyuan <18200585623@163.com>
2026-03-16 19:15:56 +0800

    修改生成字幕参数,减少碎片字幕

--- workflow/story_video_001/cases/case_kesulu_001.py
+++ workflow/story_video_001/cases/case_kesulu_001.py
@@ -7,7 +7,7 @@ from workflow.story_video_001.profiles.profile_kesulu_001 import PROFILE
 PYTHONPATH=/Users/test/code/Python/AI_vedio_demo/pythonProject \
 /Users/test/code/Python/AI_vedio_demo/pythonProject/.venv/bin/python3 \
   -m workflow.story_video_001.cases.case_kesulu_001 \
-  --input '/Users/test/Library/Mobile Documents/com~apple~CloudDocs/BEAST_BEING/my_mutimedia/my_scripts/唐僧克苏鲁/墙中之鼠/原文/03_split_md_20260212_104314/001_整篇__seg001.md'
+  --input '/Users/test/Library/Mobile Documents/com~apple~CloudDocs/BEAST_BEING/my_mutimedia/my_scripts/唐僧克苏鲁/塞勒菲斯/03_split_md_20260311_113158/001_整篇__seg003.md'
 '''
 
 if __name__ == "__main__":
